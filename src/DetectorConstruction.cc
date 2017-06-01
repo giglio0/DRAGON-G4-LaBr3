3e03c0e4bdf4b2675dcaf2e24c97c052876a7ba5
williamh2 <williamh2@users.noreply.github.com>
2017-06-01 12:21:53 -0700

    Update DetectorConstruction.cc

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -34,29 +34,6 @@
 #include "DetectorConstruction.hh"
 #include "DetectorMessenger.hh"
 
-#include "G4Material.hh"
-#include "G4Colour.hh"
-#include "G4VisAttributes.hh"
-#include "G4Box.hh"
-#include "G4Tubs.hh"
-#include "G4LogicalVolume.hh"
-#include "G4RotationMatrix.hh"
-#include "G4Transform3D.hh"
-#include "G4PVPlacement.hh"
-#include "G4UniformMagField.hh"
-
-#include "G4GeometryManager.hh"
-#include "G4PhysicalVolumeStore.hh"
-#include "G4LogicalVolumeStore.hh"
-#include "G4SolidStore.hh"
-
-#include "G4UnitsTable.hh"
-#include "G4NistManager.hh"
-#include "G4RunManager.hh"
-
-#include "G4PhysicalConstants.hh"
-#include "G4SystemOfUnits.hh"
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 DetectorConstruction::DetectorConstruction()
