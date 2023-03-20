b5de58e94224705d8d5d7b73c6efbf9dcc45c43f
williamh2 <williamh2@users.noreply.github.com>
2023-03-20 01:17:21 -0700

    Update DetectorMessenger.hh

--- include/DetectorMessenger.hh
+++ include/DetectorMessenger.hh
@@ -60,14 +60,14 @@ class DetectorMessenger: public G4UImessenger
     G4UIdirectory*             fTestemDir;
     G4UIdirectory*             fDetDir;
 
-	G4UIcmdWithADoubleAndUnit* fGapThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fGapThicknessCmd;
     G4UIcmdWithAString*        fGapMaterialCmd;
-	G4UIcmdWithADoubleAndUnit* fAlCaseThicknessCmd;
-	G4UIcmdWithADoubleAndUnit* fAlCaseDiameter1Cmd;
-	G4UIcmdWithADoubleAndUnit* fAirGapCmd;
-	G4UIcmdWithADoubleAndUnit* fPMTDiameterCmd;
-	G4UIcmdWithADoubleAndUnit* fPMTLengthCmd;
-	G4UIcmdWithAnInteger*	   fDetectorGeometryCmd;
+    G4UIcmdWithADoubleAndUnit* fAlCaseThicknessCmd;
+    G4UIcmdWithADoubleAndUnit* fAlCaseDiameter1Cmd;
+    G4UIcmdWithADoubleAndUnit* fAirGapCmd;
+    G4UIcmdWithADoubleAndUnit* fPMTDiameterCmd;
+    G4UIcmdWithADoubleAndUnit* fPMTLengthCmd;
+    G4UIcmdWithAnInteger*      fDetectorGeometryCmd;
     G4UIcmdWithoutParameter*   fUpdateCmd;
 
 };
